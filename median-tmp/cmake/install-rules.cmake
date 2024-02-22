46898ee9bab9f9eddda516fd591601b2ce823078
rslangl <fjellape1447@protonmail.com>
2024-02-22 22:22:06 +0100

    refactor: deleted old dir

--- median-tmp/cmake/install-rules.cmake
+++
@@ -1,8 +0,0 @@
-install(
-    TARGETS median_exe
-    RUNTIME COMPONENT median_Runtime
-)
-
-if(PROJECT_IS_TOP_LEVEL)
-  include(CPack)
-endif()
--- median-tmp/source/solution.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <vector>
-
-class Solution
-{
-  public:
-    static auto find_median(std::vector<int>& nums1, std::vector<int>& nums2) -> double;
-};
